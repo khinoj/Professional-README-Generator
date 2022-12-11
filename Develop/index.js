3f60038537681e2a3bedc891bfb283cdabaab3fc
khinoj <kristopher.hinojosa@gmail.com>
2022-12-11 17:23:46 -0500

    update

---
+++ Develop/index.js
@@ -0,0 +1,86 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'Titlename',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Brief description of your project',
+    },
+    {
+        type: 'input',
+        name: 'Install',
+        message: 'What are the steps to install',
+
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'What does the project/app do?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        Choose: [
+            'None',
+            'Apache License 2.0',
+            'GNU General Public License v3.0',
+            'MIT License',
+            'BSD 2-Clause "Simplified" License',
+            'BSD 3-Clause "New" or "Revised" License',
+            'Boost Software License 1.0',
+            'Creative Commons Zero v1.0 Universal',
+            'Eclipse Public License 2.0',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License v2.0',
+            'GNU Lesser General Public License v2.1',
+            'Mozilla Public License 2.0',
+            'The Unlicense',
+        ],
+        message: 'What license was assigned to project?',
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Who helped with the project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What test were done on the project?',
+    },
+    {
+        type: 'input',
+        name: 'GitHub username',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'email',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { 
+    inquirer 
+    .prompt(questions)
+    .then((answers) => {
+        const readMe = generateMarkdown
+
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
